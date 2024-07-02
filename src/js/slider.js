d930fd6aaceb94f4996ef3eac386d6f2f1f7c620
teorver <paul.britko@gmail.com>
2024-07-02 22:26:55 +0200

    feat: add swiper and footer sections

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,15 +1,66 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const swiper = new Swiper('.swiper', {
+//         direction: 'horizontal', // Default direction
+//         loop: true,
+//         slidesPerView: 3, // Show up to 4 elements
+//         spaceBetween: 20,
+//
+//         // Pagination
+//         pagination: {
+//             el: '.swiper-pagination',
+//             clickable: true,
+//         },
+//     });
+//
+//     let currentSlideIndex = 0;
+//
+//     document.querySelectorAll('.fancybox-item').forEach(item => {
+//         item.addEventListener('click', () => {
+//             currentSlideIndex = swiper.realIndex;
+//         });
+//     });
+//
+//     Fancybox.bind('[data-fancybox="gallery"]', {
+//         on: {
+//             close: () => {
+//                 swiper.slideToLoop(currentSlideIndex, 0, false);
+//                 document.activeElement.blur(); // Remove focus from the currently focused element
+//             }
+//         }
+//     });
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
-    let swiper = new Swiper('.swiper', {
-        slidesPerView: 1,
-        spaceBetween: 20,
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal', // Default direction
         loop: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+        slidesPerView: 3, // Show up to 4 elements
+        spaceBetween: 20,
+
+        // Pagination
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
         },
+    });
+
+    let currentSlideIndex = 0;
+
+    document.querySelectorAll('.fancybox-item').forEach(item => {
+        item.addEventListener('click', () => {
+            currentSlideIndex = swiper.realIndex;
+        });
+    });
+
+    Fancybox.bind('[data-fancybox="gallery"]', {
+        Thumbs: {
+            autoStart: false, // Disable thumbnails
+        },
+        on: {
+            close: () => {
+                swiper.slideToLoop(currentSlideIndex, 0, false);
+                document.activeElement.blur(); // Remove focus from the currently focused element
+            }
+        }
     });
 });
