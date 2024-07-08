bdd607a5582277591096cf44bcd9d6126f1f89f2
teorver <paul.britko@gmail.com>
2024-07-08 09:52:59 +0200

    fix 5

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // const images = document.querySelectorAll('.img__modifier');
+    
+    // images.forEach((img) => {
+    //     EXIF.getData(img, function() {
+    //         const orientation = EXIF.getTag(this, 'Orientation');
+    //         let transform = '';
+    //         switch (orientation) {
+    //             case 2: transform = 'scaleX(-1)'; break;
+    //             case 3: transform = 'rotate(180deg)'; break;
+    //             case 4: transform = 'scaleY(-1)'; break;
+    //             case 5: transform = 'rotate(90deg) scaleY(-1)'; break;
+    //             case 6: transform = 'rotate(90deg)'; break;
+    //             case 7: transform = 'rotate(270deg) scaleY(-1)'; break;
+    //             case 8: transform = 'rotate(270deg)'; break;
+    //         }
+    //         if (transform) {
+    //             img.style.transform = transform;
+    //         }
+    //     });
+    // });
+    
     const swiper = new Swiper('.swiper', {
         direction: 'horizontal', // Default direction
         loop: true,
