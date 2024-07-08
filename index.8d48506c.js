8ec9f881030ea88edda0dd7e86caa26128fcd897
teorver <paul.britko@gmail.com>
2024-07-08 09:19:30 +0200

    Updates

--- index.8d48506c.js
+++ index.8d48506c.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded",function(){let e=new Swiper(".swiper",{direction:"horizontal",loop:!0,slidesPerView:3,slidesPerGroup:3,spaceBetween:20,// Pagination
+document.addEventListener("DOMContentLoaded",function(){let e=new Swiper(".swiper",{direction:"horizontal",loop:!0,slidesPerView:3,slidesPerGroup:3,spaceBetween:40,// Pagination
 pagination:{el:".swiper-pagination",clickable:!0}});document.querySelectorAll(".fancybox-item").forEach(n=>{n.addEventListener("click",()=>{currentSlideIndex=e.realIndex})}),Fancybox.bind('[data-fancybox="gallery"]',{Thumbs:{autoStart:!1},on:{close:()=>{document.activeElement.blur();// Remove focus from the currently focused element
 }}})});
