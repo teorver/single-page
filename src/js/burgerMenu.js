dc64a2f960e6d1864ef1b323f4bd22a866e2dc75
teorver <paul.britko@gmail.com>
2024-09-02 13:59:15 +0200

    add close burger menu when item is clicked

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,9 +1,46 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
+    let header = document.querySelector('header');
+    let headerContainer = document.querySelector('.header__container');
     let burgerMenu = document.querySelector('.burger-menu');
     let navigation = document.querySelector('.navigation');
 
-    burgerMenu.addEventListener('click', function() {
+    document.addEventListener('scroll', () => {
+        let scrollPosition = window.scrollY;
+        let headerHeight = header.offsetHeight;
+
+        if (scrollPosition > headerHeight) {
+            header.classList.add('scrolled');
+            headerContainer.classList.add('scrolled-border');
+        } else {
+            header.classList.remove('scrolled');
+            headerContainer.classList.remove('scrolled-border');
+        }
+    });
+
+    burgerMenu.addEventListener('click', () => {
         navigation.classList.toggle('active');
         burgerMenu.classList.toggle('open-menu');
+    });
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            const headerOffset = document.querySelector('.header').offsetHeight;
+            const elementPosition = targetElement.getBoundingClientRect().top;
+            const offsetPosition = elementPosition - headerOffset;
+
+            window.scrollBy({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+
+            if (navigation.classList.contains('active')) {
+                navigation.classList.remove('active');
+                burgerMenu.classList.remove('open-menu');
+            }
+        });
     });
 });
