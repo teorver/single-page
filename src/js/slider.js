aa0d6040897265d79008119840e1eb4144c09fa1
teorver <paul.britko@gmail.com>
2025-03-15 12:15:59 +0100

    Update images appearance for Slider #3

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,22 +2,24 @@ $(document).ready(function() {
     $('.education-slider').flexslider({
         animation: 'slide',
         slideshow: false,
-        itemWidth: 300, // Matches SCSS base width
-        itemMargin: 40,
+        itemWidth: 300, // Base width per item
+        itemMargin: 0,  // No margin to avoid gaps
         minItems: 1,
-        maxItems: 3,
+        maxItems: 3,    // Show 3 items
+        move: 3,        // Move 3 items at a time
         controlNav: false,
         directionNav: true,
-        smoothHeight: true // Allows height to adapt
+        smoothHeight: true
     });
 
     $('.reviews-slider').flexslider({
         animation: 'slide',
         slideshow: false,
-        itemWidth: 300, // Matches SCSS base width
-        itemMargin: 50,
+        itemWidth: 300, // Base width per item
+        itemMargin: 0,  // No margin to avoid gaps
         minItems: 1,
-        maxItems: 2,
+        maxItems: 2,    // Show 2 items
+        move: 2,        // Move 2 items at a time
         controlNav: false,
         directionNav: true,
         smoothHeight: true
